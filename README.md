538a9154f27a812af71c1ba2a259d4d039bf9587
Pavel Voronin <on409949@gmail.com>
2026-06-15 16:13:38 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/pashkevi49/smart-table.git
 # sp6-2_smart-table_starter
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -6,11 +6,25 @@ export function initSorting(columns) {
         let order = null;
 
         if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+            // @todo: #3.1 — запомнить выбранный режим
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
+            // @todo: #3.2 — сбросить остальные колонки
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
         } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            // @todo: #3.3 — получить текущий режим
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
         }
 
         return sortCollection(data, field, order);
